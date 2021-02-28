fb64c299292a3fab9cde62a425f56f2c587633f7
hellowordmygosa <claricebanuelosnp68@gmail.com>
2021-02-28 23:42:03 +0800

    2.28

--- xunhuan/if.c
+++ xunhuan/if.c
@@ -2,9 +2,22 @@
 #include<Windows.h>
 int main()
 {
-
-
-
+	int age;
+	scanf("%d", &age);
+	if (age < 18)
+		printf("未成年\n");
+	else if (age>=18&&age<28)
+	{
+		printf("青年\n");
+	}
+	else if (age>=28 && age < 40)
+	{
+		printf("壮年\n");
+	}
+	else
+	{
+		printf("老爷爷\n");
+	}
 
 
 
