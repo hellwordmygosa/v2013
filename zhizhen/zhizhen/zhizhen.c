b77fe8f8383cafcf017381b3cd3984204396a011
hellowordmygosa <claricebanuelosnp68@gmail.com>
2021-02-28 15:09:47 +0800

    chuangjian

---
+++ zhizhen/zhizhen/zhizhen.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<Windows.h>
+int main()
+{
+	printf("这是一个测试程序\n");
+	printf("这是一个测试程序\n");
+	printf("这是一个测试程序\n");
+	printf("这是一个测试程序\n");
+	getchar();
+	system("pause");
+	return 0;
+}
