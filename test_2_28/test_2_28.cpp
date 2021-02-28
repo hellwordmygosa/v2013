8e49d5ddddcd30889deecd6e65d569a5fdb35f7d
hellowordmygosa <claricebanuelosnp68@gmail.com>
2021-02-28 22:30:06 +0800

    2.28

--- test_2_28/test_2_28.cpp
+++ test_2_28/test_2_28.cpp
@@ -3,12 +3,17 @@
 
 int main()
 {
+	printf("%d\n", sizeof(char*));
+	printf("%d\n", sizeof(int*));
+	printf("%d\n", sizeof(float*));
+	printf("%d\n", sizeof(double*));
 
-	int a = 20;
+
+	/*int a = 20;
 	int* p = &a;
 	printf("a=%d\n", *p);
 	*p = 66;
-	printf("a=%d\n", *p);
+	printf("a=%d\n", *p);*/
 
 
 		system("pause");
