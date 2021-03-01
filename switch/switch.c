c6b3fcdc8f31375b62f9afcc9aa74b2d1593c951
hellowordmygosa <claricebanuelosnp68@gmail.com>
2021-03-01 13:11:00 +0800

    3.1

---
+++ switch/switch.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<Windows.h>
+
+int main()
+{
+	int i = 0;
+	for (i = 1; i <= 100; i++)
+	{
+		//printf("%d\n", i);
+		if (i % 2 == 1)
+		{
+			printf("%d\n", i);
+		}
+
+
+	}
+
+	system("pause");
+	return 0;
+}
