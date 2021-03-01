4f6604bad5452d79dff2a0e296b74da564d33696
hellowordmygosa <claricebanuelosnp68@gmail.com>
2021-03-01 21:57:13 +0800

    3.1

--- while/while.c
+++ while/while.c
@@ -4,12 +4,69 @@ int main()
 
 {
 
-	char cc = 0;
-	while ((cc=getchar())!=EOF)
-	{
-		printf("%c", cc);
-				
-	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	//
+	//int ch = 0;
+	//while ((ch = getchar()) != EOF)
+	//{
+	//	if (ch<'0' || ch>'9')
+	//	{
+	//		continue;
+	//	}
+	//	putchar(ch);
+	//}
+
+	
+	//int re = 0;
+	//int ch = 0;
+	//char app[20] = { 0 };
+	//printf("请输入你的密码>-");
+
+	//scanf("%s", app);
+	//while ((ch = getchar()) != '\n')
+	//{
+	//	;
+	//}
+	////getchar();
+	//printf("请确认输入Y/N\n");
+	//
+	//re = getchar();
+	//if (re == 'Y')
+	//{
+	//	printf("输入正确\n");
+	//}
+	//else
+	//{
+	//	printf("密码输入错误\n");
+	//}
+
+
+
+
+
+
+	//char cc = 0;
+	//while ((cc=getchar())!=EOF)
+	//{
+	//	printf("%c", cc);
+	//			
+	//}
 
 
 
