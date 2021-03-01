5072733576fe9580d9da05a8a9a6beb3fd473aa1
hellowordmygosa <claricebanuelosnp68@gmail.com>
2021-03-01 15:32:24 +0800

    3.1

--- while/while.c
+++ while/while.c
@@ -1,8 +1,37 @@
 #include<stdio.h>
 #include<Windows.h>
 int main()
+
 {
 
+	char cc = 0;
+	while ((cc=getchar())!=EOF)
+	{
+		printf("%c", cc);
+				
+	}
+
+
+
+
+
+
+
+
+	//int i = 0;
+	//while (i<=10)
+	//{
+	//	if (i == 5)
+	//	{
+	//		i++;
+	//		continue;
+	//	}
+
+
+	//		
+	//	printf("%d\n", i);
+	//	i++;
+	//}
 
 
 	system("pause");
